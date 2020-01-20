7dca91b7ac3db39897123aa27e69cd28b88a6c2e
Pranav Vashi <neobuddy89@gmail.com>
2020-01-20 11:18:59 +0000

    overlay: Add Charcoal Black Primary
    Change-Id: I2d49e49b2038589b16b305689593b0867f6359c9
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -11,6 +11,7 @@ PRODUCT_PACKAGES += \
     PrimaryColorLLuviaOceanOverlay \
     PrimaryColorLLuviaNatureOverlay \
     PrimaryColorLLuviaFlameOverlay \
+    PrimaryColorLLuviaCharcoalBlackOverlay \
     EmptyOverlay \
     MatchmakerOverlay
 
