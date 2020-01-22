c20f91ed704b18105ad603555aad4a97861114f3
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-01-22 21:32:42 +0530

    overlay: Exclude overlays in this repo from enforcing RRO
    Also remove unnecessary trailing / and \
    
    Change-Id: I181a1263ab8871553243a72ffeffe5da59ef494d
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -1,5 +1,8 @@
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common/
+DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
+
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
 
 # Extra packages
 PRODUCT_PACKAGES += \
